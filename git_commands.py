b4b3dd779efa5d96e687f66e06a56cd2d008d58a
Marlen <kypchak0011@gmail.com>
2021-03-30 01:30:31 -0400

    first commit.

---
+++ git_commands.py
@@ -0,0 +1,13 @@
+git init- .git
+git remote add name url(https ot ssh key)
+git pull origin master
+git status
+git add . 
+git commit -m "comment"
+git branch
+git branch name of the branch
+git checkout name of the branch
+git push name of the branch
+git push origin master
+git reset filename
+
